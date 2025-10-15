2265ac7b06466e95d3e3bf259fe53241f5a0390d
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2025-10-15 21:02:45 +0700

    Add lesson 7

---
+++ lesson7-variable-data-types-operators/script.js
@@ -0,0 +1,51 @@
+// // var, let, const
+//
+// var hoTen = 'Nguyen Ba Tuan Anh';
+// var tuoi = 30;
+// var ketHon = true;
+// var hoTen = 'CodeGym';
+// console.log(hoTen);
+// console.log(tuoi);
+// console.log(ketHon);
+// // Cú pháp hiện đại ngày nay (let, const)
+// let hoVaTen = 'Nguyen Ba Tuan Anh';
+// hoVaTen = 'Nguyen Ba Tung Lam';
+// console.log(hoVaTen);
+// let x; // undefined
+// var y; // undefined
+// console.log(x);
+// console.log(y);
+// // let hoVaTen = 'CodeGym';
+// const PI = 3.1415926579;
+// console.log(PI);
+// const OPENAI_KEY = '';
+// const GEMINI_KEY = '';
+// const APP_OPEN_WEATHER_KEY = '';
+//
+// // Template literals
+// const firstName = `Bob's${Math.random() * 10 + 1}`;
+// console.log(firstName);
+// let salary = '1';
+// console.log(salary)
+//
+// if (salary) {
+//     console.log('Co luong');
+// }
+
+//
+// let a = 10; // number
+// let b = '10'; // string
+// console.log(a === b); // false
+
+// Toán tử tăng/giảm: ++, --
+// let x = 10;
+// let y = x++;
+// console.log(x); // 11
+// console.log(y); // 11
+
+let a = 10;
+const b = 20;
+const c = --a + b;
+console.log(a); // 9
+console.log(b); // 20
+console.log(c); // 29
